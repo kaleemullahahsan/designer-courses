bb655867093b96b1f6cf90b1d7f4d0e1a6c26381
Kaleemullah <kaleemullahahsan0@gmail.com>
2025-06-04 20:05:15 +0500

    My recent changes

--- src/Apis/Testimonials.jsx
+++ src/Apis/Testimonials.jsx
@@ -3,19 +3,19 @@ const Testimonials = [
     id: 1,
     image : "/images/elipse_1_3.png",
     name: "Laura Smith",
-    description: "..."
+    description: "ipsum dolor sit amet consectetur adipisicing elit. Enim culpa alias accusamus nam nemo nulla"
   },
   {
     id: 2,
     image : "/images/elipse_1_2.png",
     name: "Jhon Smiteh",
-    description: "..."
+    description: "ipsum dolor sit amet consectetur adipisicing elit. Enim culpa alias accusamus nam nemo nulla"
   },
   {
     id: 3,
     image : "/images/elipse_1.png",
     name: "Sara Smiteh",
-    description: "..."
+    description: "ipsum dolor sit amet consectetur adipisicing elit. Enim culpa alias accusamus nam nemo nulla"
   }
 ];
 
